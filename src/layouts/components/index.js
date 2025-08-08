381cd3e37986d83ef375ac278ef5e84d8b8a16bc
cenmen <1074850787@qq.com>
2025-08-08 12:58:06 +0800

    feat: 删除其他布局组件；

--- src/layouts/components/index.js
+++ src/layouts/components/index.js
@@ -1,6 +1,4 @@
-export { default as BeginnerGuide } from './BeginnerGuide.vue'
 export { default as BreadCrumb } from './BreadCrumb.vue'
-export { default as Fullscreen } from './Fullscreen.vue'
 export { default as MenuCollapse } from './MenuCollapse.vue'
 export { default as RoleSelect } from './RoleSelect.vue'
 export { default as SideLogo } from './SideLogo.vue'
