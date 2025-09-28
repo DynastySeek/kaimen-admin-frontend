311a818c72a1f2908650844f37290f3b7d7255aa
cenmen <1074850787@qq.com>
2025-09-28 16:17:06 +0800

    refactor: 移除RoleSelect组件并更新加载动画样式
    移除不再使用的RoleSelect组件导出
    将内联加载动画样式替换为更简洁的实现

--- src/layouts/components/index.js
+++ src/layouts/components/index.js
@@ -1,7 +1,6 @@
 export { default as BreadCrumb } from './BreadCrumb.vue';
 export { default as Header } from './Header.vue';
 export { default as MenuCollapse } from './MenuCollapse.vue';
-export { default as RoleSelect } from './RoleSelect.vue';
 export { default as Sidebar } from './Sidebar.vue';
 export { default as SideLogo } from './SideLogo.vue';
 export { default as SideMenu } from './SideMenu.vue';
