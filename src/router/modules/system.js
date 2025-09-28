ed5f89fec1ad1f83d9d8b512b27235e4248ce4cd
cenmen <1074850787@qq.com>
2025-09-28 16:03:22 +0800

    refactor(pms): 移除资源管理和角色管理相关代码
    重构系统管理模块，删除不再使用的资源管理和角色管理功能及相关组件

--- src/router/modules/system.js
+++ src/router/modules/system.js
@@ -7,7 +7,7 @@ export const systemRoutes = [
     name: 'SysMgt',
     path: '/system',
     // component: () => import('@/layouts/BaseLayout.vue'),
-    redirect: '/pms/resource',
+    redirect: '/pms/user',
     meta: {
       title: '系统管理',
       icon: 'i-fe:grid',
@@ -16,46 +16,6 @@ export const systemRoutes = [
       enable: true,
     },
     children: [
-      {
-        name: 'Resource_Mgt',
-        path: '/pms/resource',
-        component: () => import('@/views/pms/resource/index.vue'),
-        meta: {
-          title: '资源管理',
-          icon: 'i-fe:list',
-          code: 'Resource_Mgt',
-          show: true,
-          enable: true,
-        },
-      },
-      {
-        name: 'RoleMgt',
-        path: '/pms/role',
-        component: () => import('@/views/pms/role/index.vue'),
-        meta: {
-          title: '角色管理',
-          icon: 'i-fe:user-check',
-          code: 'RoleMgt',
-          show: true,
-          enable: true,
-        },
-        children: [
-          {
-            name: 'RoleUser',
-            path: '/pms/role/user/:roleId',
-            component: () => import('@/views/pms/role/role-user.vue'),
-            meta: {
-              title: '分配用户',
-              icon: 'i-fe:user-plus',
-              layout: 'full',
-              hideInMenu: true,
-              code: 'RoleUser',
-              show: false,
-              enable: true,
-            },
-          },
-        ],
-      },
       {
         name: 'UserMgt',
         path: '/pms/user',
