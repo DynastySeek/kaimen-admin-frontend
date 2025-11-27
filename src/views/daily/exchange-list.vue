2f170c0f9aa89e473955d4cfd2c23f8f180630df
Apple <apple@AppledeMacBook-Pro.local>
2025-11-27 22:26:35 +0800

    feat: 兑换明细

--- src/views/daily/exchange-list.vue
+++ src/views/daily/exchange-list.vue
@@ -207,7 +207,7 @@ const detailscolumns = [
     title: '金额',
     key: 'goldPrice',
     render: (row) => {
-      return h('div',row.type === 1 ? `+¥${row.goldPrice}` :  `-¥${row.goldPrice}` );
+      return h('div',row.type === 1 ? `-¥${row.goldPrice}` :  `+¥${row.goldPrice}` );
     },
   },
 ];
